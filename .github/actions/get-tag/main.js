31a01b6f14ad36de560d27b594fb576e501ce396
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-09 13:02:21 +0100

    Log labels

--- .github/actions/get-tag/main.js
+++ .github/actions/get-tag/main.js
@@ -1,6 +1,13 @@
 import github from '@actions/github';
 import core from '@actions/core';
 
-console.log(github.context);
+// console.log(github.context.payload.);
+if (github.context.eventName === 'pull_request') {
+	let { labels } = github.context.payload.pull_request;
+	console.log(labels);
+	// if (labels.includes('major')) {
+	// 	core.setOutput('version', 'major');
+	// } else if (labels.includes('minor')) {}
+}
 
 core.setOutput('version', '1.0.1');
